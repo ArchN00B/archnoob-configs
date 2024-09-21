01043eb0fadd4ee430bb261c27309efbe5c8315e
ArchN00B <archn00b2024@gmail.com>
2024-09-21 11:44:55 -0400

    deleted a script i didnt need

--- pushit2git.sh
+++ pushit2git.sh
@@ -1,52 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 #set -e
 ##################################################################################################################
-# Author    : ArchN00B
+# Author 	: ArchN00B
 # Website   : https://www.github.com/ArchN00B
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-#tput setaf 0 = black
-#tput setaf 1 = red
+#tput setaf 0 = black 
+#tput setaf 1 = red 
 #tput setaf 2 = green
-#tput setaf 3 = yellow
-#tput setaf 4 = dark blue
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
 #tput setaf 5 = purple
-#tput setaf 6 = cyan
-#tput setaf 7 = gray
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
 #tput setaf 8 = light blue
 ##################################################################################################################
 
-# checking if I have the latest files from github
-echo "Checking for updated pushed files"
-git pull
-
-# Below command will backup everything inside the project folder
-git add --all .
-
-# Give a comment to the commit if you want
-
 echo "####################################"
-echo "Write your commit comment!"
+echo "#### Checking for updated files "
 echo "####################################"
+git pull 
+
+
+
+git add .
+sleep 3
+git status 
+
+echo "####################################################" 
+echo "##### Adding files to resprotory make your commit "
+echo "####################################################"
 
 read input
-
-# Committing to the local repository with a message containing the time details and commit text
-
 git commit -m "$input"
 
-# Push the local files to github
+echo "####################################################"
+echo "######### Finishing up all done "
+echo "####################################################"
 
-if grep -q main .git/config; then
-	echo "Using main"
-		git push -u origin main
-fi
+git push
 
-if grep -q master .git/config; then
-	echo "Using master"
-		git push -u origin master
-fi
 
-echo "################################################################"
-echo "###################    Git Push Done      ######################"
-echo "################################################################"
+
+
+
